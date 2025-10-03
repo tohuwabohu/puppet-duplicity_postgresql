5c24035eeb671b1208b6ded330b2854377993dee
Martin Meinhold <Martin.Meinhold@gmx.de>
2025-10-03 19:31:38 +0200

    Puppet 8 compatibility

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,32 +11,32 @@
 # Copyright 2014 Martin Meinhold, unless otherwise noted.
 #
 class duplicity_postgresql::params {
-  $backup_dir = $::osfamily ? {
+  $backup_dir = $facts['os']['family'] ? {
     default => '/var/backups/postgresql'
   }
 
   $dump_script_template = 'duplicity_postgresql/usr/local/sbin/dump-postgresql-database.sh.erb'
-  $dump_script_path = $::osfamily ? {
+  $dump_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/dump-postgresql-database.sh'
   }
 
   $check_script_template = 'duplicity_postgresql/usr/local/sbin/check-postgresql-database.sh.erb'
-  $check_script_path = $::osfamily ? {
+  $check_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/check-postgresql-database.sh'
   }
 
   $restore_script_template = 'duplicity_postgresql/usr/local/sbin/restore-postgresql-database.sh.erb'
-  $restore_script_path = $::osfamily ? {
+  $restore_script_path = $facts['os']['family'] ? {
     default => '/usr/local/sbin/restore-postgresql-database.sh'
   }
 
-  $postgresql_client_package_name = $::osfamily ? {
+  $postgresql_client_package_name = $facts['os']['family'] ? {
     default => 'postgresql-client'
   }
-  $grep_package_name = $::osfamily ? {
+  $grep_package_name = $facts['os']['family'] ? {
     default => 'grep'
   }
-  $gzip_package_name = $::osfamily ? {
+  $gzip_package_name = $facts['os']['family'] ? {
     default => 'gzip'
   }
 }
